8d7302cf159b2d893b83cb2549fa23a35fbb8f9f
EgleNavickiene <navickiene@gmail.com>
2021-10-19 17:15:47 +0300

    Product photo

--- src/app/products.ts
+++ src/app/products.ts
@@ -3,6 +3,7 @@ export interface Product {
   name: string;
   price: number;
   description: string;
+  photo: string;
 }
 
 export const products = [
@@ -10,19 +11,22 @@ export const products = [
     id: 1,
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    photo: 'https://lt3.pigugroup.eu/colours/456/239/33/45623933/apple-iphone-13-128gb-blue_xbig.jpg'
   },
   {
     id: 2,
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    photo: 'https://lt1.pigugroup.eu/colours/347/381/66/34738166/apple-iphone-12-mini-64gb-white-kaina_xbig.jpg'
   },
   {
     id: 3,
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: '',
+    photo: 'https://lt2.pigugroup.eu/colours/314/919/26/31491926/apple-iphone-se-2020-128gb-black-price_xbig.jpg'
   }
 ];
 
